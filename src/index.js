560d832dd473bc0f76bb8a7ec4d01caa06d1d0e2
MrDrog <fireman3085@mail.ru>
2022-05-21 00:21:41 +0300

    feature: filter by price

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import renderPage from "./modules/renderPage";
 import search from "./modules/search"
 import catalog from './modules/catalog'
+import filter from './modules/filter'
 
 renderPage();
 search();
 catalog();
+filter();
